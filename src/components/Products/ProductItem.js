b9d433950560d5dd6811ba5f5c4a9d21d978db2c
vimala-siravi <vimala1995@gmail.com>
2023-08-24 13:01:50 +0530

    Added product list and items component

---
+++ src/components/Products/ProductItem.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+function ProductItem({ details }) {
+  return (
+    <li className="py-5">
+      <div className="flex min-w-0 gap-x-4">
+        <img
+          className="h-12 w-12 flex-none rounded-full"
+          src={details.thumbnail}
+          alt={details.title}
+        />
+        <div className="min-w-0 flex-auto">
+          <p className="text-sm font-semibold leading-6">{details.title}</p>
+          <p className="mt-1 truncate text-xs leading-5">
+            {details.description}
+          </p>
+        </div>
+        <div className="hidden shrink-0 sm:flex sm:flex-col sm:items-end">
+          <p className="text-sm leading-6">{details.brand}</p>
+          <p className="mt-1 text-xs leading-5">{details.price}</p>
+        </div>
+      </div>
+    </li>
+  );
+}
+
+export default ProductItem;
---
+++ src/components/Products/ProductList.js
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from "react";
+import ProductItem from "./ProductItem";
+
+function ProductList() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
+  const fetchProducts = async () => {
+    const response = await fetch(
+      `https://dummyjson.com/products?limit=${process.env.REACT_APP_PRODUCTS_LIMIT}`
+    );
+    const data = await response.json();
+    if (data?.products) {
+      setProducts(data.products);
+    }
+  };
+
+  return (
+    <div>
+      <ul className="divide-y divide-gray-100">
+        {products?.map((product) => {
+          return <ProductItem details={product} key={product.id} />;
+        })}
+      </ul>
+    </div>
+  );
+}
+
+export default ProductList;
